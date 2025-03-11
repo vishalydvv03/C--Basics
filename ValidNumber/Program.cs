3d568dc19fdb94893e508a8b40ff7b5ca0a1d145
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-11 18:11:01 +0530

    Added new project

---
+++ ValidNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 to 10 : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num>=1 && num <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
